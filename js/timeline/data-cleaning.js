c5de14baedc8568d7444767fbffc23c4c4eb2b1e
Jeff <jeffrey.miiller@gmail.com>
2020-03-22 02:06:06 -0600

    Add the cleaned democratic debate data.

---
+++ js/timeline/data-cleaning.js
@@ -0,0 +1,9 @@
+// I used this to clean up the data.
+
+/*
+d3.text('data/timeline/DemocraticPrimaryDebateSchedule.csv', d3.autotype).then(data => {
+    const regex = /\[\d{3}\]/g;
+
+    console.log(data.replace(regex, ""));
+});
+*/
