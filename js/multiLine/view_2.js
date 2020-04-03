cf6fa8772f4b6ef197f8a367d5af97050c83a0a6
Roger Yu-Hsiang Lo <b13472003@gmail.com>
2020-04-03 09:14:51 -0700

    Updated data in view_2.js

--- js/multiLine/view_2.js
+++ js/multiLine/view_2.js
@@ -1,12 +1,12 @@
 import { MultiLine } from "./multiLine.js";
 
-d3.csv('data/NYT_data.csv', d3.autotype).then(data => {
+d3.csv('data/NYT_data_average.csv', d3.autotype).then(data => {
     data.forEach((row) => {
-        row[`Date`] = new Date (row[`Date`]);
-        row[`SentScore(headline)`] = +row[`SentScore(headline)`];
-        row[`SentScore(abstract)`] = +row[`SentScore(abstract)`];
-        row[`SentScore(lead)`] = +row[`SentScore(lead)`];
-        row[`SentScore(Avg)`] = +row[`SentScore(Avg)`];
+        row['Date'] = new Date (row['Date']);
+        row['SentScore(headline)'] = +row['SentScore(headline)'];
+        row['SentScore(abstract)'] = +row['SentScore(abstract)'];
+        row['SentScore(lead)'] = +row['SentScore(lead)'];
+        row['SentScore(Avg)'] = +row['SentScore(Avg)'];
     });
     //console.log(data);
     const multiLine = new MultiLine(data, {
