1067fd87284b20a50f84f749efb91ec2d9a0021b
Roger Yu-Hsiang Lo <b13472003@gmail.com>
2020-04-04 14:01:39 -0700

    Add view 3 to scrollytelling.html

--- js/control.js
+++ js/control.js
@@ -1,77 +1,9 @@
-// initialize chart
-let bubble;
-
-// load data
-d3.csv('data/NYT_data.csv').then(data => {
-    data.forEach(d => {
-        d['SentScore(Avg)'] = +d['SentScore(Avg)'];
-        d['SentScore(abstract)'] = +d['SentScore(abstract)'];
-        d['SentScore(headline)'] = +d['SentScore(headline)'];
-        d['SentScore(lead)'] = +d['SentScore(lead)'];
-        d['Pub_date'] = d['Date'];
-        d['Date'] = new Date(d['Date']);
-        d['Count'] = +d['Count'];
-        d['id'] = Math.random();
-    });
-
-    bubble = new Bubble({
-        parentElement: '#bubble',
-        containerWidth: 840,
-        containerHeight: 600
-    }, data);
-
-    bubble.group = 'all';
-
-    bubble.update();
-});
-
-$('#politics').click(() => {
-    if($('input[id=politics]').is(':checked')) {
-        bubble.categories.add('politics');
-    } else {
-        bubble.categories.delete('politics');
-    }
-    bubble.update();
-});
-
-$('#opinion').click(() => {
-    if($('input[id=opinion]').is(':checked')) {
-        bubble.categories.add('opinion');
-    } else {
-        bubble.categories.delete('opinion');
-    }
-    bubble.update();
-});
-
-$('#business').click(() => {
-    if($('input[id=business]').is(':checked')) {
-        bubble.categories.add('business');
-    } else {
-        bubble.categories.delete('business');
-    }
-    bubble.update();
-});
-
-$('#other').click(() => {
-    if($('input[id=other]').is(':checked')) {
-        bubble.categories.add('other');
-    } else {
-        bubble.categories.delete('other');
-    }
-    bubble.update();
-});
-
-$('input[type=radio]').click(() => {
-    const radioValue = $('input[name=category]:checked').val();
-    bubble.group = radioValue;
-    bubble.update();
-});
-
 const div1 = () => {
     d3.select('#widget').classed('hidden', false);
 
     d3.select('#bubble').classed('hidden', false);
     d3.select('#multiLine').classed('hidden', true);
+    d3.select('#timeLine').classed('hidden', true);
 
     $('input[id=politics]').prop('checked', true);
     $('input[id=opinion]').prop('checked', true);
@@ -95,6 +27,7 @@ const div2 = () => {
 
     d3.select('#bubble').classed('hidden', false);
     d3.select('#multiLine').classed('hidden', true);
+    d3.select('#timeLine').classed('hidden', true);
 
     $('input[id=politics]').prop('checked', true);
     $('input[id=opinion]').prop('checked', true);
@@ -119,6 +52,7 @@ const div3 = () => {
 
     d3.select('#bubble').classed('hidden', false);
     d3.select('#multiLine').classed('hidden', true);
+    d3.select('#timeLine').classed('hidden', true);
 
     $('input[id=politics]').prop('checked', false);
     $('input[id=opinion]').prop('checked', true);
@@ -143,6 +77,15 @@ const div4 = () => {
 
     d3.select('#bubble').classed('hidden', true);
     d3.select('#multiLine').classed('hidden', false);
+    d3.select('#timeLine').classed('hidden', true);
+};
+
+const div5 = () => {
+    d3.select('#widget').classed('hidden', true);
+
+    d3.select('#bubble').classed('hidden', true);
+    d3.select('#multiLine').classed('hidden', true);
+    d3.select('#timeLine').classed('hidden', false);
 };
 
 const scroll = (n, offset, func1, func2) => {
@@ -159,3 +102,4 @@ const scroll = (n, offset, func1, func2) => {
 scroll('div2', '75%', div2, div1);
 scroll('div3', '75%', div3, div2);
 scroll('div4', '75%', div4, div3);
+scroll('div5', '75%', div5, div4);
