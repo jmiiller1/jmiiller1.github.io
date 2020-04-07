afdb1e51e8b0de8ada8047d1f5ca50fb7dc8fe77
Roger Yu-Hsiang Lo <b13472003@gmail.com>
2020-04-06 21:56:31 -0700

    Added default selection when users deselect

--- js/timeline/timeline-brush.js
+++ js/timeline/timeline-brush.js
@@ -7,13 +7,14 @@ export class TimelineBrush {
         brushGroup.call(brushX);
     }
 
-    static createBrushX(height, width, timeScale, dispatcher){
+    static createBrushX(height, width, timeScale, dispatcher) {
 
         const moveBrush = TimelineBrush.moveBrush(timeScale, dispatcher);
 
         const brushX = d3.brushX();
         brushX.extent([[0, height - 20], [width, height + 20]]);
         brushX.on('brush', moveBrush);
+        brushX.on('end', moveBrush);
 
         return brushX
     }
@@ -22,6 +23,9 @@ export class TimelineBrush {
         return function() {
             if (d3.event.selection) {
                 const highlightedArea = d3.event.selection.map(timeScale.invert);
+                dispatcher.call('focus', this, highlightedArea);
+            } else {
+                const highlightedArea = [new Date(2019, 5, 1), new Date(2020, 2, 1)];
                 dispatcher.call('focus', this, highlightedArea);
             }
         }
