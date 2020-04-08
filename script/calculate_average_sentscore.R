ca8e2a2e195c06822aa4cbd75d642566ea99e265
Roger Yu-Hsiang Lo <b13472003@gmail.com>
2020-04-08 01:11:32 -0700

    Updated NYT_data.csv and NYT_data_average.csv to include more months

--- script/calculate_average_sentscore.R
+++ script/calculate_average_sentscore.R
@@ -1,14 +1,16 @@
 library(tidyverse)
 
+setwd("~/Documents/CPSC436/Milestone/project_democratic-primary/data")
+
 df <- read.csv("NYT_data.csv", header = TRUE, sep = ",")
 d <- df %>%
   mutate(Date = lubridate::ymd(Date),
          Week = lubridate::floor_date(Date, unit = "week", week_start = 1)) %>%
   group_by(Candidates, Week) %>%
   summarise(SentScore.headline = mean(SentScore.headline.),
-            SentScore.abstract = mean(SentScore.abstract.),
+            SentScore.snippet = mean(SentScore.snippet.),
             SentScore.lead = mean(SentScore.lead.),
-            SentScore.avg = mean(SentScore.Avg.),
+            SentScore.avg = mean(SentScore.avg.),
             n = n())
 
 write.csv(d, file = "NYT_data_average.csv", quote = TRUE, row.names = FALSE)
--- script/nyt_api.py
+++ script/nyt_api.py
@@ -9,62 +9,66 @@ from collections import Counter
 
 analyzer = SentimentIntensityAnalyzer()
 
-#api_key = '3vBom6ktWDJ9iFa3jXvmcx1JhVfQH79z'
+api_key = '3vBom6ktWDJ9iFa3jXvmcx1JhVfQH79z'
 
-#for year, month in [(2019, 6), (2019, 7), (2019, 8), (2019, 9), (2019, 10), (2019, 11), (2019, 12), (2020, 1), (2020, 2)]:
-#    print(year, month)
-#    url = f'https://api.nytimes.com/svc/archive/v1/{year}/{month}.json?api-key=' + api_key
-#    r = requests.get(url)
-#    json_data = r.json()
+for year, month in [(2018, 12),
+                    (2019, 1), (2019, 2), (2019, 3), (2019, 4), (2019, 5), (2019, 6), (2019, 7), (2019, 8), (2019, 9), (2019, 10), (2019, 11), (2019, 12),
+                    (2020, 1), (2020, 2), (2020, 3), (2020, 4)]:
+    print(year, month)
+    url = f'https://api.nytimes.com/svc/archive/v1/{year}/{month}.json?api-key=' + api_key
+    r = requests.get(url)
+    json_data = r.json()
 
-#    filename = f'{year}-{month}.txt'
-#    output = open(filename, mode='w')
-#    json.dump(json_data, output)
-#    output.close()
-#    time.sleep(20)
+    filename = f'../data/{year}-{month}.txt'
+    output = open(filename, mode='w')
+    json.dump(json_data, output)
+    output.close()
+    time.sleep(20)
 
-out = open('../data/NYT_data.csv', mode='w')
+out = open('../data/NYT_data_new.csv', mode='w')
 writer = csv.writer(out, quoting=csv.QUOTE_NONNUMERIC)
-writer.writerow(['Date', 'Candidates', 'NewsDesk', 'Category', 'Headline', 'SentScore(headline)', 'SentScore(abstract)', 'SentScore(lead)', 'SentScore(Avg)', 'Count'])
-#out.write(','.join(['Date', 'Candidates', 'Section', 'SentScore(headline)', 'SentScore(abstract)', 'SentScore(lead)', 'SentScore(Avg)', 'Count']) + '\n')
+writer.writerow(['Date', 'Candidates', 'NewsDesk', 'Category', 'Headline', 'SentScore(headline)', 'SentScore(snippet)', 'SentScore(lead)', 'SentScore(avg)', 'Count'])
 
 ids = set()
 
-for year, month in [(2019, 6), (2019, 7), (2019, 8), (2019, 9), (2019, 10), (2019, 11), (2019, 12), (2020, 1), (2020, 2)]:
+for year, month in [(2018, 12),
+                    (2019, 1), (2019, 2), (2019, 3), (2019, 4), (2019, 5), (2019, 6), (2019, 7), (2019, 8), (2019, 9), (2019, 10), (2019, 11), (2019, 12),
+                    (2020, 1), (2020, 2), (2020, 3), (2020, 4)]:
+    print(year, month)
     f = open(f'../data/{year}-{month}.txt', mode='r')
     articles = json.load(f)
-    #print(json.dumps(articles, indent=4, sort_keys=True))
 
     # Joe Biden, Bernie Sanders, Elizabeth Warren, Michael Bloomberg, Pete Buttigieg
     for i in range(len(articles['response']['docs'])):
         headline = articles['response']['docs'][i]['headline']['main']
-        abstract = articles['response']['docs'][i]['abstract']
-        lead_paragraph = articles['response']['docs'][i]['lead_paragraph']
+        #abstract = articles['response']['docs'][i]['abstract']
+        lead_paragraph = articles['response']['docs'][i].get('lead_paragraph', None)
         keywords = [obj['value'] for obj in articles['response']['docs'][i]['keywords']]
-        section_name = articles['response']['docs'][i]['news_desk'].lower()
+        section_name = articles['response']['docs'][i].get('news_desk', '').lower()
         date = articles['response']['docs'][i]['pub_date'][:10]
         snippet = articles['response']['docs'][i]['snippet']
         count = articles['response']['docs'][i]['word_count']
 
         id = articles['response']['docs'][i]['_id']
 
-        if sum([name in headline for name in ['Biden', 'Sanders', 'Warren', 'Bloomberg', 'Buttigieg']]) == 1 and int(count) > 0 and id not in ids:
+        if headline and sum([name in headline for name in ['Biden', 'Sanders', 'Warren', 'Bloomberg', 'Buttigieg']]) == 1 and int(count) > 0 and id not in ids:
             names = choice(
                 [name for name in ['Biden', 'Sanders', 'Warren', 'Bloomberg', 'Buttigieg'] if name in headline])
             #names = ','.join([name for name in ['Biden', 'Sanders', 'Warren', 'Bloomberg', 'Buttigieg'] if name in headline])
             ids.add(id)
             score_headline = analyzer.polarity_scores(headline)['compound']
-            score_abstract = analyzer.polarity_scores(abstract)['compound']
-            score_paragraph = analyzer.polarity_scores(lead_paragraph)['compound']
-            avg = (score_headline + score_abstract + score_paragraph) / 3
+            score_snippet = analyzer.polarity_scores(snippet)['compound']
+            score_paragraph = analyzer.polarity_scores(lead_paragraph)['compound'] if lead_paragraph else -100
+            avg = (score_headline + score_snippet + score_paragraph) / 3 if lead_paragraph else (score_headline + score_snippet) / 2
+
             if section_name in {'business', 'business day', 'sundaybusiness'}:
                 category = 'business'
             elif section_name in {'editorial', 'opinion', 'oped', 'upshot'}:
                 category = 'opinion'
-            elif section_name in {'politics', 'national', 'u.s.', 'washington'}:
+            elif section_name in {'politics', 'national', 'u.s.', 'washington', 'u.s. / politics'}:
                 category = 'politics'
             else:
                 category = 'other'
-            writer.writerow([date, names, section_name, category, headline, round(score_headline, 2), round(score_abstract, 2), round(score_paragraph, 2), round(avg, 2), count])
+            writer.writerow([date, names, section_name, category, headline, round(score_headline, 2), round(score_snippet, 2), round(score_paragraph, 2), round(avg, 2), count])
 
 out.close()
